b2b7907d016c4ca7f649b1bb1fa6f225bd50c733
TumVink <ge54xof@mytum.de>
2023-11-15 12:48:12 +0100

    add H100 test

--- result/boxplot.py
+++ result/boxplot.py
@@ -9,11 +9,11 @@ def read_numpy_array(file_path):
 
 
 # Function to plot boxplot
-def plot_and_save_boxplot(data1, data2, save_path):
+def plot_and_save_boxplot(data1, data2,data3, save_path):
     fig, ax = plt.subplots()
-    ax.boxplot([data1, data2])
+    ax.boxplot([data1, data2,data3])
 
-    ax.set_xticklabels(['RTX3090', 'A4500'])
+    ax.set_xticklabels(['RTX3090', 'A4500','H100'])
     ax.set_ylabel('Time / [s]')
     ax.set_title('Training Time on PatchCam per GPU')
     bot,top = ax.get_ylim()
@@ -30,16 +30,18 @@ if __name__ == "__main__":
     # Replace 'path_to_array1.npy' and 'path_to_array2.npy' with the actual file paths
     array1_path = '3090_run=10.npy'
     array2_path = 'A4500_run=10.npy'
+    array3_path = 'H100_run=10.npy'
 
     # Read numpy arrays from files
     array1 = read_numpy_array(array1_path)
     array2 = read_numpy_array(array2_path)
+    array3 = read_numpy_array(array3_path)
     #print(array2.shape)
 
     # Ensure that the arrays have the correct shape [1, 10]
     if array1.shape == (10,) and array2.shape == (10,):
-        save_path = 'PatchCam_4500vs3090.png'
+        save_path = 'PatchCam_4500vs3090vsH100.png'
         # Plot and save the boxplot
-        plot_and_save_boxplot(array1[:], array2[:], save_path)
+        plot_and_save_boxplot(array1[:], array2[:],array3[:], save_path)
     else:
         print("Error: Arrays should have shape [10,].")
